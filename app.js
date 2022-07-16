746d7d9d6e82f0a07983f1262db5af69f9a74fd2
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-07-16 15:18:28 +0800

    Add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 
 const router = require("./routes");
@@ -9,6 +10,8 @@ const swaggerJSON = require("./docs/openapi.json");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

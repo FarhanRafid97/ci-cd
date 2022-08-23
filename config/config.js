fbfbf82ba490babddd19a691c298e1db2457a72b
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-08-23 22:23:57 +0800

    Fix production config

--- config/config.js
+++ config/config.js
@@ -18,5 +18,13 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };

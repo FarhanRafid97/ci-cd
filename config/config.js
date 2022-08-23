6d50717ec90255bd9dcf2cb48adf655b9a04a7ae
farhan <farhanrafid97@gmail.com>
2022-08-23 21:26:21 +0700

    make it better

--- config/config.js
+++ config/config.js
@@ -1,25 +1,30 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_NAME}_development`,
-    host: "localhost",
-    dialect: "postgres",
+    host: 'localhost',
+    dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_NAME}_test`,
-    host: "localhost",
-    dialect: "postgres",
+    host: 'localhost',
+    dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    host: "localhost",
-    dialect: "postgres",
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };

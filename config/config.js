d54d9c78f404c0725e0756e3e3a69eafe1430aae
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-08-23 22:21:09 +0800

    Add release phase and change production config for Sequelize

--- config/config.js
+++ config/config.js
@@ -16,10 +16,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    host: "localhost",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
   },
 };

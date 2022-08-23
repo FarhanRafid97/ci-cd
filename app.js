9cf29e1e8652b9688a7505fe716290f2a79e9385
farhan <farhanrafid97@gmail.com>
2022-08-23 20:30:35 +0700

    make it better

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const swaggerJSON = require('./docs/openapi.json');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 

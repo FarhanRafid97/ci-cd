fddb3865eaa57066985a4ab9cdc9227f9e97c264
farhan <farhanrafid97@gmail.com>
2022-09-01 20:10:11 +0700

    add secret

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-<<<<<<< HEAD
-=======
-    jest: true,
->>>>>>> 09ee8b0e22cc1f5fdf3c097599ead789a8e004cd
-  },
-  extends: ['airbnb-base', 'prettier'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {},
-};
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+        jest: true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    rules: {},
+}
